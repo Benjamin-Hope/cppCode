a909ac7c06c026ae5bbde1c9602dfdc01861cc98
Benjamin-Hope <97188208+Benjamin-Hope@users.noreply.github.com>
2023-12-30 03:48:34 +0000

    helloWorld

---
+++ SimpleHelloWorld/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream> //this includes the labrary for input and output
+
+/*multi line
+comments*/
+
+int main() { //main function 
+	//the code will be run sequentially 
+	std::cout << "Hello World\n"; //std::count is a character output stream
+	
+}
+
+/*To complite the code in the terminal we can write
+"g++ {filename}.cpp" and to execute wirite after "./a.out"*/
+
+/*We can name the file when compiling '#kinda like alias', by doing
+g++ {filename}.cpp -o {alias} and then run with just "./{alias}*/
